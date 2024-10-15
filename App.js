18d2cd9c42545e2e65326dca2d40e282dd4927a5
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-10-15 21:54:58 +0800

    little change ✅

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{ uri: "https://majuberkarya.site//login" }} />
+      <WebView source={{ uri: "https://majuberkarya.site/login" }} />
     </SafeAreaView>
   );
 }
